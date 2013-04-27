95c09fecca6cd5b07679f789e578ed0a0f107a1a
Ben <ben@Bens-MacBook-Pro.local>
2013-04-27 14:24:47 -0700

    add email string

--- kangdouangnhot_banchop_assignment4.js
+++ kangdouangnhot_banchop_assignment4.js
@@ -1,18 +1,48 @@
 //alert("JavaScript works!");
 
+// String telephone number and String e-mail boolean
 
+var phone = function ( ) {
+
+
+// telephone variables
 var teleNumber = "407-695-0100";
-var end =   teleNumber.indexOf( "-" );
+var end 	   =   teleNumber.indexOf( "-" );
+var start      = teleNumber.indexOf( "-" );
+var ends       = teleNumber.lastIndexOf("-");
 
-var start = teleNumber.indexOf( "-" );
-var stop = teleNumber.lastIndexOf("-");
 
-console.log(teleNumber.substring(  0, end + start + stop ));
-  
-   
-if ( teleNumber === "407-695-0100" ) {
+// email variables
+var email      = "ben@fullsail.com";
+var endMail        = email.indexOf( "@" );
+var startMail     = email.indexOf( "." );
+var stopMail       = email.lastIndexOf(".");
+
+
+// telephone boolean  
+	if ( teleNumber === "407-695-0100" ) {
+		console.log(teleNumber.substring(0, end + start + ends));
 		console.log("String passes");
-} else {
-		console.log("Does Not pass!!!");
+		
+		} else {
+
+			console.log("Does Not pass!!!");
+		
+	}
+
+// email boolean
+		if ( email === "ben@fullsail.com" ) {
+		console.log(email.substring( 0, endMail + startMail + stopMail ));
+		console.log("String passes");
+		
+		} else {
+
+			console.log("Does Not pass!!!");
+		
+	}
 
 };
+
+phone();
+
+
