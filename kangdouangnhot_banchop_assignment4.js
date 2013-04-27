c4bc151abfa8629e0b6e640636ca64d85ff28429
Ben <ben@Bens-MacBook-Pro.local>
2013-04-27 12:50:37 -0700

    tele string pattern

--- kangdouangnhot_banchop_assignment4.js
+++ kangdouangnhot_banchop_assignment4.js
@@ -1,1 +1,18 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+
+var teleNumber = "407-695-0100";
+var end =   teleNumber.indexOf( "-" );
+
+var start = teleNumber.indexOf( "-" );
+var stop = teleNumber.lastIndexOf("-");
+
+console.log(teleNumber.substring(  0, end + start + stop ));
+  
+   
+if ( teleNumber === "407-695-0100" ) {
+		console.log("String passes");
+} else {
+		console.log("Does Not pass!!!");
+
+};
