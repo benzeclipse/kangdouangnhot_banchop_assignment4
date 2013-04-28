0913f1d12f2fd4abc711fbaa200e22048c5f6b92
Ben <ben@Bens-MacBook-Pro.local>
2013-04-27 21:06:09 -0700

    cleaningup code

--- kangdouangnhot_banchop_assignment4.js
+++ kangdouangnhot_banchop_assignment4.js
@@ -6,42 +6,48 @@
 
 
 
+
+
+
 // String telephone 
 
 var myTelePhone = function ( ) {
 
 	// telephone variables
 	var teleNumber    = "407-695-0100";
-
 	var endArea       = teleNumber.indexOf( "-" );
-
 	var startTele     = teleNumber.indexOf( "-" )+1;
-
 	var endMiddle     = teleNumber.lastIndexOf( "-" );
-
 	var sLast         = teleNumber.lastIndexOf( "-" )+1;
-
 	var endsTele      = teleNumber.length;
-
 	
 // telephone boolean  
 
-if ( teleNumber === "407-695-0100" ) {
-	
-	var a = teleNumber.substring (0, endArea );
-	var b = teleNumber.substring ( startTele, endMiddle );
-	var c = teleNumber.substring ( sLast , endsTele );
+if ( teleNumber === "407-695-0100" ) {	
+		var a = teleNumber.substring (0, endArea );
+		var b = teleNumber.substring ( startTele, endMiddle );
+		var c = teleNumber.substring ( sLast , endsTele );
 
-	console.log( a , b , c );
+		console.log( a , b , c );
 		
-		} else {
+	} else {
 
-			console.log("Does Not pass!!!");
+		console.log("Does Not pass!!!");
 		
 	}
 
-
 };
+
+
+
+
+
+
+
+
+
+
+
 
 // e-mail String
 
@@ -50,23 +56,15 @@ var eMail = function () {
 // email variables
 
 	var email         = "ben@fullsail.com";
-
 	var endMail       = email.indexOf( "@" );
-
 	var startMail     = email.indexOf( "@" )+1;
-
 	var endMails      = email.indexOf( "." );
-
 	var startLast     = email.indexOf( "." )+1;
-
 	var endsMail      = email.length;
-
-	
-
 
 // email boolean
 
-	if ( email === "ben@fullsail.com" ) {
+if ( email === "ben@fullsail.com" ) {
 			var d = email.substring(0, endMail );
 		    var e = email.substring( startMail, endMails );
 		    var f = email.substring( startLast, endsMail);
@@ -81,35 +79,47 @@ var eMail = function () {
 };
 
 
-// add URL string
+
+
+
+
+
+
+
+
+// Checking URL string
 
 var urlString = function ( ) {
-
 	var urlA = "http://";
 	var urlB = "https://";
 	
-if ( urlA === "http://" ) {
+	if ( urlA === "http://" ) {
 	
 			console.log("You may pass");
 	
 		}else {
 		
-		console.log("can't pass");
+			console.log("can't pass");
 	
 };
 	
-if ( urlB = "https://" ) {
+	if ( urlB = "https://" ) {
 	
-		console.log("You may pass");
+			console.log("You may pass");
 	
-	} else {
+		} else {
 	
-		console.log("can't pass");
+			console.log("can't pass");
 	
 	}
 
-
 };
+
+
+
+
+
+
 
 
 // Number Functions passing in two parameters
@@ -120,18 +130,30 @@ var numberString = function( first, finalNum ){
 
 	console.log( first + finalNum );
 
-
 };
 
 
+
+
+
+
+
+// Title case string
 // passing in arguments and changing the beginning of each word to Capital
 
 var capitalString = function ( h, w ) {
-
 	console.log(h.replace ( "h", "H" ) );
 	console.log(w.replace ( "w", "W" ) );
 
 };
+
+
+
+
+
+
+
+
 
 
 
